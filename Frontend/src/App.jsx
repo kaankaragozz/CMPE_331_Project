3c22a870ac33d215b3b3b28ae067dae91f438274
Baris Unluer <unluerbaris@gmail.com>
2025-11-29 11:55:04 +0300

    Add RosterTabularPage

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import FlightSelectionPage from "./pages/Flights/FlightSelectionPage.jsx";
 import CrewAssignmentPage from "./pages/Flights/CrewAssignmentPage.jsx";
 import SeatAssignmentPage from "./pages/Passengers/SeatAssignmentPage.jsx";
 import PlaneSeatMapPage from "./pages/Roster/PlaneSeatMapPage.jsx";
+import RosterTabularPage from "./pages/Roster/RosterTabularPage.jsx";
 
 export default function App() {
   return (
@@ -16,6 +17,7 @@ export default function App() {
         <Route path="/flights/:flightId/crew" element={<CrewAssignmentPage />} />
         <Route path="/flights/:flightId/passengers" element={<SeatAssignmentPage />} />
         <Route path="/roster/:flightId/plane" element={<PlaneSeatMapPage />} />
+        <Route path="/roster/:flightId/tabular" element={<RosterTabularPage />} />
       </Routes>
     </AppLayout>
   );
